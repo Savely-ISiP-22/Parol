223a81d04b2cbed6e3dc22135e4253d96f444d64
Savely-ISiP-22 <66845696+Savely-ISiP-22@users.noreply.github.com>
2020-06-13 00:28:15 +0500

    Add files via upload

---
+++ Пароль/Подбор пароля/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtB1 = new System.Windows.Forms.TextBox();
+            this.btnProverka = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtB1
+            // 
+            this.txtB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtB1.Location = new System.Drawing.Point(45, 70);
+            this.txtB1.Multiline = true;
+            this.txtB1.Name = "txtB1";
+            this.txtB1.Size = new System.Drawing.Size(153, 34);
+            this.txtB1.TabIndex = 0;
+            this.txtB1.TextChanged += new System.EventHandler(this.txtB1_TextChanged);
+            // 
+            // btnProverka
+            // 
+            this.btnProverka.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnProverka.Location = new System.Drawing.Point(60, 130);
+            this.btnProverka.Name = "btnProverka";
+            this.btnProverka.Size = new System.Drawing.Size(125, 33);
+            this.btnProverka.TabIndex = 1;
+            this.btnProverka.Text = "Проверить";
+            this.btnProverka.UseVisualStyleBackColor = true;
+            this.btnProverka.Click += new System.EventHandler(this.btnProverka_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(60, 186);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 33);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl1.Location = new System.Drawing.Point(41, 24);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(157, 24);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "Введите пароль";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 247);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnProverka);
+            this.Controls.Add(this.txtB1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Проверка пароля";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtB1;
+        private System.Windows.Forms.Button btnProverka;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Пароль/Подбор пароля/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string password;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            {
+                string text = txtB1.Text;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.FileName = "password.txt";
+                saveFileDialog1.Filter = "txt files (.txt)|.txt|All files (.)|.";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string filename = saveFileDialog1.FileName;
+                    File.WriteAllText(filename, text);
+                    MessageBox.Show("Файл сохранен");
+                }
+            }
+        }
+
+        private void btnProverka_Click(object sender, EventArgs e)
+        {
+            password = Convert.ToString("1234");
+            try
+            {
+                
+            }
+            
+            finally
+            {
+                btnSave.Enabled = true;
+                if (txtB1.Text == password)
+                { MessageBox.Show("Пароль верный"); }
+            
+            
+                else
+                {
+                    btnSave.Enabled = false;
+                    MessageBox.Show("Пароль не верный"); 
+                }
+        }
+    }
+
+        private void txtB1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
