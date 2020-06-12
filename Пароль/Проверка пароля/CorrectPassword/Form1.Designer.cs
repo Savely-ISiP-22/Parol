cfbb11e255fb49016929312d192764f16a01f170
Savely-ISiP-22 <66845696+Savely-ISiP-22@users.noreply.github.com>
2020-06-13 00:36:26 +0500

    Add files via upload

---
+++ Пароль/Проверка пароля/CorrectPassword/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CorrectPassword
+{
+    partial class EnterPass
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblEnterPass = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BackColor = System.Drawing.Color.Moccasin;
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPassword.Location = new System.Drawing.Point(29, 48);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(212, 25);
+            this.tbPassword.TabIndex = 0;
+            // 
+            // lblEnterPass
+            // 
+            this.lblEnterPass.AutoSize = true;
+            this.lblEnterPass.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblEnterPass.Location = new System.Drawing.Point(49, 22);
+            this.lblEnterPass.Name = "lblEnterPass";
+            this.lblEnterPass.Size = new System.Drawing.Size(168, 23);
+            this.lblEnterPass.TabIndex = 1;
+            this.lblEnterPass.Text = "Введите пароль.";
+            // 
+            // btnOK
+            // 
+            this.btnOK.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PapayaWhip;
+            this.btnOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOK.Location = new System.Drawing.Point(98, 83);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(81, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // EnterPass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(266, 121);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblEnterPass);
+            this.Controls.Add(this.tbPassword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EnterPass";
+            this.Text = "Пароль";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblEnterPass;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Пароль/Проверка пароля/CorrectPassword/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorrectPassword
+{
+    public class WrongPasswordException : Exception
+    {
+        public WrongPasswordException(String message) : base(message)
+        { }
+
+        public WrongPasswordException(String message, Exception inner) : base(message, inner) { }
+    }
+
+    public partial class EnterPass : Form
+    {
+        public EnterPass()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessString(string password)
+        {
+            bool isUpper = false;
+            
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == password.ToUpper()[i])
+                {
+                    isUpper = true;
+                }
+            }
+
+            bool isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] >= '0' && password[i] <= '9')
+                {
+                    isNumber = true;
+                }
+            }
+          
+            if 
+            (
+           !(password.Contains("!")||
+            password.Contains("@")||
+            password.Contains("#")||
+            password.Contains("$")||
+            password.Contains("%")||
+            password.Contains("^")))
+            {
+                throw new WrongPasswordException("WrongPassword");
+            }
+            if (isUpper == false)
+            {
+                throw new WrongPasswordException("WrongPassword");
+            }
+            else if (isNumber == false)
+            {
+                throw new WrongPasswordException("WrongPassword");
+            }
+            else if (password.Length < 6)
+            {
+                throw new WrongPasswordException("WrongPassword");
+            }
+        }
+
+        private void TryCatch()
+        {
+            string password = tbPassword.Text;
+            try
+            {
+                ProcessString(password);
+                File.WriteAllText("password.txt", password);
+                MessageBox.Show("Пароль записан.", "", MessageBoxButtons.OK);
+            }
+            catch (WrongPasswordException)
+            {
+                MessageBox.Show("ПАРОЛЬ ВВЕДЁН НЕКОРРЕКТНО\nМинимум один из данных символов: !, @, #, $, %, ^\nМинимум 6 символов\nМинимум 1 прописная буква\nМинимум 1 цифра","НЕКОРРЕКТНЫЙ ВВОД",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error); ;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            TryCatch();
+        }
+    }
+}
